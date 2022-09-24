6b1c4ffad90fd22cb5b7ba06e3ee38c62e722880
Brayan-g15 <b.sleider15@gmail.com>
2022-09-24 08:19:35 -0500

    error en db

--- src/db.js
+++ src/db.js
@@ -1,16 +1,30 @@
-import { createPool } from "mysql2/promise"
+import mysql from 'mysql'
+import { createPool } from 'mysql'
+
 import {
-    DB_HOST,
-    DB_NAME,
-    DB_PASSWORD,
-    DB_USER,
-    DB_PORT
-  } from './config.js'
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT
+} from './config.js'
+
+
 
 export const conection = createPool({
     host : DB_HOST,
     user : DB_USER,
     password : DB_PASSWORD,
     database : DB_NAME,
-    port: DB_PORT,
+    port: DB_PORT
 })
+
+// conexion.connect( (error)=> {
+//     if(error){
+//         console.log('El error de conexión es: '+error)
+//         return
+//     }
+//     console.log('¡Conectado a la base de datos MySQL!')
+// })
+
+// module.exports = conection
